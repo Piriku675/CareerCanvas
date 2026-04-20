c36c1989d19dd59146cd39a2aedc5181434b3427
Anthony Kuiau <akuiau@proton.me>
2026-04-20 23:41:00 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,11 +10,6 @@ import { getFirestore, collection, doc, getDocs, getDoc, query, orderBy }
 
 // ─── YOUR Firebase config ─────────────────────────────────────────────────────
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC2PtWHIKYLZx_hQuYhScoMeOr0rqq2DB8",
